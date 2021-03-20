88516e9eb9fb0fbe76c3ce19486a13e421f46ae4
zaidallam <75961407+zaidallam@users.noreply.github.com>
2021-03-20 18:07:10 -0500

    Completed Log Navigation

--- client/src/components/post_auth/Logs.js
+++ client/src/components/post_auth/Logs.js
@@ -7,16 +7,18 @@ import { Log } from './helper_components/logs'
 import axios from 'axios'
 import '../../css/post_auth/Logs.css'
 import '../../css/post_auth/General.css'
-import { set } from 'js-cookie'
 
 export const Logs = () => {
 
     const { isAuth, logout, authUser} = useContext(Context);
-    const [workoutLog, setWorkoutLog] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
+    const [sourceLog, setSourceLog] = useState([]);
+    const [logPool, setLogPool] = useState([])
     const [activeLog, setActiveLog] = useState([]);
+
+    const [totalPages, setTotalPages] = useState(1);
     const [selectedPage, setSelectedPage] = useState(1);
+
+    const [selectedDateRange, setSelectedDateRange] = useState({from: '', to: ''})
 
     const fetchWorkoutLog = () => {
         axios({
@@ -25,7 +27,7 @@ export const Logs = () => {
             url: `http://localhost:5000/users/${authUser}?resource=logs`
         })
         .then((res) => {
-            setWorkoutLog(res.data.reverse());
+            setSourceLog(res.data.reverse());
         })
         .catch(() => {
             console.log('fetching failed');
@@ -37,13 +39,57 @@ export const Logs = () => {
     }, []);
 
     useEffect( () => {
-        setActiveLog(workoutLog.slice((currentPage - 1)*9, (currentPage - 1)*9+9));
-        setTotalPages(Math.ceil(workoutLog.length / 9));
-    }, [workoutLog] );
+        setLogPool(sourceLog)
+        setActiveLog(sourceLog.slice(0, 9));
+        setTotalPages(Math.ceil(sourceLog.length / 9));
+    }, [sourceLog] );
 
-    useEffect( () => {
-        setActiveLog(workoutLog.slice((currentPage - 1)*9, (currentPage - 1)*9+9));
-    }, [currentPage] );
+    const switchPage = (page) => {
+        setActiveLog(logPool.slice((page - 1)*9, (page - 1)*9+9));
+    }
+
+    const applyDateRange = () => {
+        if (!selectedDateRange.from || !selectedDateRange.to) {
+            return null;
+        }
+        const from = new Date(selectedDateRange.from);
+        const to = new Date(selectedDateRange.to);
+        let matchingWorkouts = [];
+        matchingWorkouts = sourceLog.filter( workout => {
+            let workoutDate = new Date(workout.date);
+            return (workoutDate >= from && workoutDate <= to);
+        });
+        matchingWorkouts.sort((a, b) => {
+            let c = new Date(a.date);
+            let d = new Date(b.date);
+            return c-d;
+        });
+        matchingWorkouts.reverse()
+        setLogPool(matchingWorkouts);
+        setActiveLog(matchingWorkouts.slice(0, 9));
+        Array.from(document.querySelectorAll("#page-select")).forEach(
+            input => (input.value = 1)
+        );
+        setSelectedPage(1);
+        setTotalPages(Math.ceil(matchingWorkouts.length/9))
+    }
+
+    const clearDateRange = () => {
+        setLogPool(sourceLog)
+        setActiveLog(sourceLog.slice(0, 9));
+        setTotalPages(Math.ceil(sourceLog.length / 9));
+        Array.from(document.querySelectorAll("#page-select")).forEach(
+            input => (input.value = 1)
+        );
+        setSelectedPage(1);
+        Array.from(document.querySelectorAll("#from-date")).forEach(
+            input => (input.value = '')
+        );
+        Array.from(document.querySelectorAll("#to-date")).forEach(
+            input => (input.value = '')
+        );
+        setSelectedDateRange({from: '', to: ''});
+    }
 
     if (isAuth) {
         return (
@@ -54,14 +100,14 @@ export const Logs = () => {
                     <h2>LOGS</h2>
                     <div className="filters flex-row">
                         <label htmlFor="from-date">FROM:</label><br className="mobile-only" />
-                        <input type="date" id="from-date" name="from-date" /><br className="mobile-only" />
+                        <input type="date" id="from-date" name="from-date" onChange={ e => setSelectedDateRange({ from: e.target.value, to: selectedDateRange.to }) }/><br className="mobile-only" />
                         <label htmlFor="to-date">TO:</label><br className="mobile-only" />
-                        <input type="date" id="to-date" name="to-date" />
-                        <button>APPLY</button><br className="mobile-only" />
+                        <input type="date" id="to-date" name="to-date" onChange={ e => setSelectedDateRange({ from: selectedDateRange.from, to: e.target.value }) }/>
+                        <button onClick={ e => { e.preventDefault(); applyDateRange() } }>APPLY</button><button onClick={ e => { e.preventDefault(); clearDateRange() } }>CLEAR</button><br className="mobile-only" />
                         <label htmlFor="from-date">PAGE</label>
-                        <input type="number" id="page-select" name="page-select" defaultValue='1' onChange={ e => setSelectedPage(e.target.value)}/>
+                        <input type="number" id="page-select" name="page-select" defaultValue='1' onChange={ e => setSelectedPage(e.target.value)} />
                         <label htmlFor="from-date">OF {totalPages}</label>
-                        <button onClick={ e => { e.preventDefault(); setCurrentPage(selectedPage) } }>GO</button>
+                        <button onClick={ e => { e.preventDefault(); switchPage(selectedPage) } }>GO</button>
                     </div>
                     <div className="logs-list grid-3">
 
